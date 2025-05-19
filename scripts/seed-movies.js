78139146ce7d5816e587497ee41cc14cc1efca69
joaohlviana <joaohlviana@gmail.com>
2025-05-19 11:32:42 -0300

    Setup Meilisearch with movie data

---
+++ scripts/seed-movies.js
@@ -0,0 +1,36 @@
+import { MeiliSearch } from 'meilisearch';
+import movies from '../src/data/movies.json';
+
+const client = new MeiliSearch({
+  host: 'http://localhost:7700',
+  apiKey: 'masterKey' // Default master key if not changed
+});
+
+async function seedMovies() {
+  try {
+    // Create movies index
+    const index = client.index('movies');
+    
+    // Add documents to the index
+    const response = await index.addDocuments(movies.movies);
+    console.log('Movies added successfully:', response);
+
+    // Configure searchable attributes
+    await index.updateSearchableAttributes([
+      'title',
+      'overview'
+    ]);
+
+    // Configure filterable attributes
+    await index.updateFilterableAttributes([
+      'year',
+      'rating'
+    ]);
+
+    console.log('Meilisearch setup completed successfully!');
+  } catch (error) {
+    console.error('Error seeding movies:', error);
+  }
+}
+
+seedMovies();

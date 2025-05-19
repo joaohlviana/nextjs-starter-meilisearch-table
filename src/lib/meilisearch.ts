fab870a756a5612d759984ce520e39152a8720b5
joaohlviana <joaohlviana@gmail.com>
2025-05-19 12:14:27 -0300

    Fix MeiliSearch client and configuration

--- src/lib/meilisearch.ts
+++ src/lib/meilisearch.ts
@@ -9,11 +9,25 @@ class MeilisearchClient {
   private constructor() {
     const host = process.env.NEXT_PUBLIC_MEILISEARCH_URL;
     const apiKey = process.env.NEXT_PUBLIC_MEILISEARCH_SEARCH_KEY;
-
     this.client = new MeiliSearch({ 
       host: host || 'http://localhost:7700',
       apiKey: apiKey || 'masterKey'
     });
+  }
+
+  public static getInstance(): MeilisearchClient {
+    if (!MeilisearchClient.instance) {
+      MeilisearchClient.instance = new MeilisearchClient();
+    }
+    return MeilisearchClient.instance;
+  }
+
+  public getIndex(index: string): Index {
+    return this.client.index(index);
+  }
+
+  public async health() {
+    return this.client.health();
   }
 }
 
